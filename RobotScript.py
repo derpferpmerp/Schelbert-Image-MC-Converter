3e1fbb7e688ca481c1f110152382ee2d6a23f64e
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2022-02-09 08:19:43 -0600

    Create RobotScript.py

---
+++ RobotScript.py
@@ -0,0 +1,25 @@
+import random
+
+L = []
+
+def beta(x):
+    return x
+
+for x_i in range(10):
+	tmp = []
+	for y_i in range(10):
+		CHOICE = bool(random.randint(0, 1))
+		L.append([x_i, y_i, True])
+
+LS = sorted(L, key=lambda x: x[1])
+CX, CZ, CY = beta(agent.position)
+agent.say(str(CX))
+for x, y, delete in L:
+	agent.say(str(CX+x))
+	agent.say(str(CY+y))
+	agent.teleport([CX+x, CZ, CY+y])
+	#player.teleport([CX+x, CZ, CY+y])
+	if delete:
+		agent.destroy("DOWN")
+
+
